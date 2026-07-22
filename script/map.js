616f8b516f970de4c44c057d27a679c1336e6fd3
elouan660 <elouan660.deschamps@proton.me>
2026-07-22 16:47:47 +0300

    try

--- script/map.js
+++ script/map.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([56.9449216,24.0642629], 13);
+var map = L.map('map').setView([56.9866054, 24.0799902], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([56.9449216,24.0642629]).addTo(map)
-    .bindPopup('FixPrice')
+L.marker([56.9866054, 24.0799902]).addTo(map)
+    .bindPopup('Spilve airport')
     .openPopup();
